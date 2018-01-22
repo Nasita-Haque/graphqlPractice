325cae93c423c40e79860bfa487a5d896d6dbebd
Nasita Haque <nasita.haque@gmail.com>
2018-01-21 19:18:12 -0500

    Update to ES6

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
-var schema = buildSchema(`
+const schema = buildSchema(`
   type Query {
     hello: String
   }
 `);
 
-var root = {
+const root = {
   hello: () => {
     return 'Hello world!';
   },
 };
 
-var app = express();
+const app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
 }));
+
 app.listen(4000);
+
 console.log('Running a GraphQL API server at localhost:4000/graphql');
